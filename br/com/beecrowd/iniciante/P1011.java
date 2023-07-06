7dc71eb8a9ace63c3a8d7d8908c177887f2a7807
Hygor Luciano <128493484+HygorLucianoGoncalves@users.noreply.github.com>
2023-07-06 12:52:37 -0300

    Create P1011.java

---
+++ br/com/beecrowd/iniciante/P1011.java
@@ -0,0 +1,13 @@
+package br.com.beecrowd.iniciante;
+
+import java.util.Scanner;
+
+public class P1011 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double r = teclado.nextDouble();
+        double pi = 3.14159;
+        double valor = (4.0/3)*pi*Math.pow(r,3);
+        System.out.printf("VOLUME = %.3f\n",valor);
+    }
+}
