c556749b9f739fe2ebc22c5008809c655c926450
Hygor Luciano <128493484+HygorLucianoGoncalves@users.noreply.github.com>
2023-07-06 15:48:43 -0300

    Create P1014.java

---
+++ br/com/beecrowd/iniciante/P1014.java
@@ -0,0 +1,15 @@
+package br.com.beecrowd.iniciante;
+
+import java.util.Scanner;
+
+public class P1014 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int x;
+        double y,total;
+        x = teclado.nextInt();
+        y = teclado.nextDouble();
+        total = x/y;
+        System.out.printf("%.3f km/l",total);
+    }
+}
