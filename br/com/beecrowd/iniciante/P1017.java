76fce7239686168db8b5b23ed219b1bf95218648
Hygor Luciano <128493484+HygorLucianoGoncalves@users.noreply.github.com>
2023-07-06 17:41:54 -0300

    Create P1017.java

---
+++ br/com/beecrowd/iniciante/P1017.java
@@ -0,0 +1,16 @@
+package br.com.beecrowd.iniciante;
+
+import java.util.Scanner;
+
+public class P1017 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double horas,km;
+        horas = teclado.nextInt();
+        km = teclado.nextInt();
+        double total = (km/12)*horas;
+        System.out.printf("%.3f\n",total);
+        teclado.close();
+
+    }
+}
