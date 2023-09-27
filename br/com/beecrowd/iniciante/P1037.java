8d2465f4bf841c493ad5501cff8132671f3b486a
Hygor Luciano <hygorluciano0@gmail.com>
2023-09-26 22:06:50 -0300

    1037 OK

---
+++ br/com/beecrowd/iniciante/P1037.java
@@ -0,0 +1,25 @@
+package br.com.beecrowd.iniciante;
+
+import java.util.Scanner;
+
+public class P1037 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double entrada = scanner.nextDouble();
+
+        if (entrada < 0 || entrada > 100){
+            System.out.println("Fora de intervalo");
+        } else if (entrada >= 0 && entrada <= 25.00) {
+            System.out.println("Intervalo [0,25]");
+        } else if (entrada > 25.00 && entrada <= 50.00){
+            System.out.println("Intervalo (25,50]");
+        } else if (entrada > 50.00 && entrada <= 75.00){
+            System.out.println("Intervalo (50,75]");
+        } else{
+            System.out.println("Intervalo (75,100]");
+        }
+
+        scanner.close();
+    }
+}
