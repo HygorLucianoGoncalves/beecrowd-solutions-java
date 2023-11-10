83a7dd2d2d60bb5875d3246b7a04c9850404a471
Hygor Luciano <hygorluciano0@gmail.com>
2023-11-10 14:11:55 -0300

    P 1044 OK

--- br/com/beecrowd/iniciante/P1044.java
+++ br/com/beecrowd/iniciante/P1044.java
@@ -1,2 +1,22 @@
-package br.com.beecrowd.iniciante;public class P1044 {
+package br.com.beecrowd.iniciante;
+
+import java.util.Scanner;
+
+public class P1044 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int a,b;
+        a = teclado.nextInt();
+        b = teclado.nextInt();
+
+        boolean saoMultiplos = b % a == 0 || a % b == 0;
+
+        if (saoMultiplos) {
+            System.out.println("Sao Multiplos");
+        } else {
+            System.out.println("Nao sao Multiplos");
+        }
+
+    }
 }
