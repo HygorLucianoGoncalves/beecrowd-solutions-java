f3329661b853fc929aff6930926d70cffa29a9da
Hygor Luciano <hygorluciano0@gmail.com>
2024-11-08 17:06:28 -0300

    testando fazer o problema 1023

--- br/com/beecrowd/java/estruturasebibliotecas/P1023.java
+++ br/com/beecrowd/java/estruturasebibliotecas/P1023.java
@@ -15,34 +15,32 @@ em seguida deve-se listar, por ordem ascendente de consumo, a quantidade de pess
 Na terceira linha da saída deve-se mostrar o consumo médio por pessoa da cidade, com 2 casas decimais sem arredondamento, considerando o consumo real total.
  Imprimir uma linha em branco entre dois casos de teste consecutivos. No fim da saída não deve haver uma linha em branco.*/
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int quantidadeDeImoveis;
-
-
-        ArrayList<Integer> listQuantidadeDeMoradores = new ArrayList<Integer>();
-        ArrayList<Integer> listConsumoDoImovel = new ArrayList<Integer>();
-        Collections.sort(listConsumoDoImovel);
+        HashMap<Integer, Integer> listEstiagem = new HashMap<Integer, Integer>();
+        int cidade = 0;
 
         while (true) {
-            int quantidadeDeMoradoes;
-            int consumoDoimovel;
+            Scanner scanner = new Scanner(System.in);
+
+            int quantidadeDeImoveis, quantidadeDeMoradoes, consumoDoimovel, div;
+
             quantidadeDeImoveis = scanner.nextInt();
             if (quantidadeDeImoveis == 0) {
                 break;
             }
 
             for (int i = 0; i < quantidadeDeImoveis; i++) {
-                listQuantidadeDeMoradores.add(quantidadeDeMoradoes = scanner.nextInt());
-                listConsumoDoImovel.add(consumoDoimovel = scanner.nextInt() / quantidadeDeMoradoes);
+                quantidadeDeMoradoes = scanner.nextInt();
+                consumoDoimovel = scanner.nextInt();
+                div = consumoDoimovel / quantidadeDeMoradoes;
+                listEstiagem.put(quantidadeDeMoradoes, div);
+                System.out.println(listEstiagem);
 
             }
-            int cidade = 0;
-
             cidade++;
 
         }
-
-        System.out.println(listQuantidadeDeMoradores);
-        System.out.println(listConsumoDoImovel);
+        for (int i = 1; i <= cidade; i++) {
+            System.out.println("Cidade# " + i + ":");
+        }
     }
 }
