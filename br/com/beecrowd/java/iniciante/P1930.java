a83d9a7e77660cc36ebf2cc0e5f980852e839bac
Hygor Luciano <hygorluciano0@gmail.com>
2024-11-11 10:47:17 -0300

    P 1930 OK

---
+++ br/com/beecrowd/java/iniciante/P1930.java
@@ -0,0 +1,18 @@
+package br.com.beecrowd.java.iniciante;
+
+import java.util.Scanner;
+
+public class P1930 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int T1,T2,T3,T4;
+
+        T1 = scanner.nextInt();
+        T2 = scanner.nextInt();
+        T3 = scanner.nextInt();
+        T4 = scanner.nextInt();
+
+        System.out.println((T1 + T2 + T3 + T4)-3);
+    }
+}
