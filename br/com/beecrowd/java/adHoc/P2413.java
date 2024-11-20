dca4645c7a58894551b20bd7610e0ff3824001da
Hygor Luciano <hygorluciano0@gmail.com>
2024-11-20 20:54:15 -0300

    P 2413 OK

--- br/com/beecrowd/java/adHoc/P2413.java
+++ br/com/beecrowd/java/adHoc/P2413.java
@@ -1,4 +1,17 @@
 package br.com.beecrowd.java.adHoc;
 
+import java.util.Scanner;
+
 public class P2413 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int primiroLink,segundoLink;
+        int terceiroLink = sc.nextInt();
+
+        segundoLink = terceiroLink * 2;
+        primiroLink = segundoLink * 2;
+
+        System.out.println(primiroLink);
+        sc.close();
+    }
 }
