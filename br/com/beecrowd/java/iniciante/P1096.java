fd2d4beca25acd4cd37e6df38d7768aec51be01e
Hygor Luciano <hygorluciano0@gmail.com>
2025-02-26 10:47:06 -0300

    P 1096 OK

---
+++ br/com/beecrowd/java/iniciante/P1096.java
@@ -0,0 +1,13 @@
+package br.com.beecrowd.java.iniciante;
+
+public class P1096 {
+    public static void main(String[] args) {
+        int a = 1;
+        for (int i =0; i <=4; i++){
+            System.out.println("I="+a+" J=7");
+            System.out.println("I="+a+" J=6");
+            System.out.println("I="+a+" J=5");
+            a += 2;
+        }
+    }
+}
