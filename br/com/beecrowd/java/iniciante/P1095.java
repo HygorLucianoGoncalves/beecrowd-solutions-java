f583cf7be4e5d6ea6cbfb63fc6f1918e77bec60f
Hygor Luciano <hygorluciano0@gmail.com>
2025-03-05 10:35:48 -0300

    P 1097 OK

---
+++ br/com/beecrowd/java/iniciante/P1095.java
@@ -0,0 +1,18 @@
+package br.com.beecrowd.java.iniciante;
+
+public class P1095 {
+    public static void main(String[] args) {
+        int teste = 1;
+      for (int j = 60; j >= 0; j--){
+          if (j % 5 == 0 ){
+
+              System.out.println(teste);
+              System.out.println(j);
+              teste = teste + 3;
+          }
+
+
+      }
+
+    }
+}
